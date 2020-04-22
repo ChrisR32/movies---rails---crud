752d5b2f011ef1fb9c8ff06287eaabf7e7a34d91
ChrisR32 <59920367+ChrisR32@users.noreply.github.com>
2020-04-23 07:28:08 +1000

    index styling

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "/movies", to: "movies#index" , as: "movies"
   post "/movies", to: "movies#create"
   get "movies/new", to: "movies#new"
-  get "/movies/:id/edit", to: "movies#edit"
+  get "/movies/:id/edit", to: "movies#edit", as: "edit"
   get "/movies/:id", to: "movies#show", as: "movie"
   put "/movies/:id", to: "movies#update"
   delete "/movies/:id", to: "movies#destroy"
